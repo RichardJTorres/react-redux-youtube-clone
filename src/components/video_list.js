2e0f588789b586aec3837f387c6dfb773f6e4aae
Rich Torres <richjtorres@gmail.com>
2016-07-08 15:51:29 -0700

    adding video selection callback

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,7 +3,12 @@ import VideoListItem from './video_list_item'
 
 const VideoList = (props) => {
   const videoItems = props.videos.map((video) => {
-    return <VideoListItem key={video.eta} video={video} />
+    return (
+      <VideoListItem
+        onVideoSelect={props.onVideoSelect}
+        key={video.eta}
+        video={video} />
+    )
   });
 
   return (
